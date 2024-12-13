f9a7dbddf2602526df06f7c5a78928f49d7bdcfd
Liran Shunak <liranshunak@Lirans-MacBook-Pro.local>
2024-12-13 16:14:09 +0200

    some changes

--- backend/tests/mocks/emailServices.js
+++ backend/tests/mocks/emailServices.js
@@ -3,3 +3,7 @@ jest.mock('../../services/email', () => ({
   sendVerificationEmail: jest.fn().mockResolvedValue(true),
   generateVerificationCode: jest.fn().mockReturnValue('123456')
 }));
+
+jest.mock('dotenv', () => ({
+  config: jest.fn()
+}));
